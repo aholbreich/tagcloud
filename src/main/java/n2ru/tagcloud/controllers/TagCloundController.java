67699dd01d41011ccc3763bc3d4f71c27c5344ff
shuron <holbreich@googlemail.com>
2016-02-18 00:32:46 +0100

    better sizes

--- src/main/java/n2ru/tagcloud/controllers/TagCloundController.java
+++ src/main/java/n2ru/tagcloud/controllers/TagCloundController.java
@@ -17,8 +17,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class TagCloundController {
 
-	private static final int TAG_MAX_FONT_SIZE = 36;
-	private static final int TAG_MIN_FONT_SIZE = 10;
+	private static final int TAG_MAX_FONT_SIZE = 72;
+	private static final int TAG_MIN_FONT_SIZE = 11;
 
 
 	public static final Logger LOG = LoggerFactory.getLogger(Application.class);
@@ -31,18 +31,20 @@ public class TagCloundController {
 	@RequestMapping("/")
 	public String getTagCloud(Model model) {
 		LOG.info("Todo app");
-		List<Tag> tags = em.createNativeQuery("SELECT * from pligg_tag_cache limit 100", Tag.class).getResultList();
+		List<Tag> tags = em.createNativeQuery("SELECT * from pligg_tag_cache limit 50", Tag.class).getResultList();
 
 		Tag mostused= tags.stream().max((t1, t2) -> Integer.compare( t1.getCount(), t2.getCount())).get(); 
 		//Tag lessused= tags.stream().min((t1, t2) -> Integer.compare( t1.getCount(), t2.getCount())).get(); 
 		int gap = TAG_MAX_FONT_SIZE - TAG_MIN_FONT_SIZE;
-		
+		LOG.info("gap {}, most {}", gap, mostused.getCount());
 		for(Tag tag :tags){
-			tag.setFontsize(TAG_MIN_FONT_SIZE + tag.getCount()/mostused.getCount()*gap);
+			double sizePlus = (((double)tag.getCount()/(double)mostused.getCount() )*(double)gap);
+			LOG.info("coeff {} , Size plus {}", (tag.getCount()/mostused.getCount() ), sizePlus);
+			tag.setFontsize(TAG_MIN_FONT_SIZE+(int)sizePlus);
 		}
 		
 		model.addAttribute("name", "testname");
-		model.addAttribute("tags_max_pts",TAG_MAX_FONT_SIZE);
+		model.addAttribute("tags_max_pts",36);
 		model.addAttribute("tags",tags );
 		return "cloud";
 	}
--- src/main/java/n2ru/tagcloud/domain/Tag.java
+++ src/main/java/n2ru/tagcloud/domain/Tag.java
@@ -4,6 +4,7 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.persistence.Transient;
 
 @Entity()
 @Table(name="pligg_tag_cache")
@@ -13,6 +14,7 @@ public class Tag {
 	@Column(name = "tag_words")
 	private String words;
 	private Integer count;
+	@Transient
 	private int fontsize;
 	
 	public String getWords() {
