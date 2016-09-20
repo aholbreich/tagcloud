dcad511846f0807114d3dac287c234a12eecda01
shuron <holbreich@googlemail.com>
2016-09-21 00:07:48 +0200

    improving Query

--- src/main/java/n2ru/tagcloud/service/TagDao.java
+++ src/main/java/n2ru/tagcloud/service/TagDao.java
@@ -12,7 +12,15 @@ import org.springframework.transaction.annotation.Transactional;
 public interface TagDao extends CrudRepository<Tag, Long> {
 
 	// http://docs.spring.io/spring-data/data-jpa/docs/current/reference/html/#jpa.repositories
-	@Query(value = "SELECT * from pligg_tag_cache limit 60", nativeQuery = true)
+	//@Query(value = "SELECT * from pligg_tag_cache limit 60", nativeQuery = true)
+	
+	//select tag_words, count(DISTINCT link_id) as count 
+	//FROM pligg_tags , pligg_links .  
+	//WHERE tag_lang='ru' and link_id = tag_link_id and (link_status='published' OR link_status='queued')  order by count desc limit
+	@Query(value = "SELECT tag_words, count(DISTINCT link_id) as count "
+			+ "FROM pligg_tags , pligg_links "
+			+ "WHERE tag_lang='ru' and link_id = tag_link_id and (link_status='published' OR link_status='queued') "
+			+ "ORDER BY count desc limit 60", nativeQuery = true)
 	public List<Tag> getTops();
 
 }
